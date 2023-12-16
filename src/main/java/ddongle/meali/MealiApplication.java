8f28259a1c3f14cc16e7c69e6f0747d2aeedf43e
sunghyun <a91078933@gmail.com>
2023-12-16 20:13:15 +0900

    서블릿 실습

---
+++ src/main/java/ddongle/meali/MealiApplication.java
@@ -0,0 +1,15 @@
+package ddongle.meali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan //서블릿 자동 등록
+@SpringBootApplication
+public class MealiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ddongle/meali/MealiApplicationTests.java
@@ -0,0 +1,13 @@
+package ddongle.meali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
