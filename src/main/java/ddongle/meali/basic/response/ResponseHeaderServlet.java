37e0d532320c4130fef7dc802419d51f2e7a5d91
sunghyun <a91078933@gmail.com>
2023-12-25 18:44:04 +0900

    responseHeaderServlet

--- src/main/java/ddongle/meali/basic/response/ResponseHeaderServlet.java
+++ src/main/java/ddongle/meali/basic/response/ResponseHeaderServlet.java
@@ -11,6 +11,7 @@ import java.io.PrintWriter;
 
 @WebServlet(name = "responseHeaderServlet", urlPatterns = "/response-header")
 public class ResponseHeaderServlet extends HttpServlet {
+
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //[status-line
