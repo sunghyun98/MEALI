e476ca2e3f70b66a632245f9902914b415a63894
sunghyun <a91078933@gmail.com>
2024-01-28 23:05:44 +0900

    2024-01-28

--- src/main/java/ddongle/meali/wep/frontcontroller/v4/FrontControllerServletV4.java
+++ src/main/java/ddongle/meali/wep/frontcontroller/v4/FrontControllerServletV4.java
@@ -36,7 +36,6 @@ public class FrontControllerServletV4 extends HttpServlet {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return;
         }
-
         //paramMap
         Map<String, String> paramMap = createParamMap(request);
         Map<String, Object> model = new HashMap<>();//추가2
