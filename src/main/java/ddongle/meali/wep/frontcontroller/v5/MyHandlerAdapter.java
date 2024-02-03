b5967f8e47a0c390e0fb8696676a0983d2351b34
sunghyun <a91078933@gmail.com>
2024-02-03 19:49:43 +0900

    2024-02-03

---
+++ src/main/java/ddongle/meali/wep/frontcontroller/v5/MyHandlerAdapter.java
@@ -0,0 +1,15 @@
+package ddongle.meali.wep.frontcontroller.v5;
+
+import ddongle.meali.wep.frontcontroller.ModelView;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public interface MyHandlerAdapter {
+    boolean supports(Object handler);
+
+    ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException;
+
+}
--- src/main/java/ddongle/meali/wep/frontcontroller/v5/te.java
+++
@@ -1,5 +0,0 @@
-package ddongle.meali.wep.frontcontroller.v5;
-
-public class te {
-    int a = 13;
-}
