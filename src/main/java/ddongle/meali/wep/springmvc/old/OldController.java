490d19371ed67439c8e164e21797dac96d260cb6
sunghyun <a91078933@gmail.com>
2024-02-08 23:55:03 +0900

    2024-02-08

--- src/main/java/ddongle/meali/wep/springmvc/old/OldController.java
+++ src/main/java/ddongle/meali/wep/springmvc/old/OldController.java
@@ -7,12 +7,13 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
 //spring been 이름
+
 @Component("/springmvc/old-controller")
 public class OldController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("OldController.handleRequest");
-        return null;
+        return new ModelAndView("new-form");
 
     }
 }
