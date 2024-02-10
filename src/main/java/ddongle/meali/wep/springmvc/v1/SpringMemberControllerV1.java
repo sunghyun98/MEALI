b1066e3ffcdd923180623b9a7d50813d6682030b
sunghyun <a91078933@gmail.com>
2024-02-10 23:26:17 +0900

    2024-02-10

--- src/main/java/ddongle/meali/wep/springmvc/v1/SpringMemberControllerV1.java
+++ src/main/java/ddongle/meali/wep/springmvc/v1/SpringMemberControllerV1.java
@@ -19,6 +19,7 @@ public class SpringMemberControllerV1 {
         String username = paramMap.get("username");
         int age = Integer.parseInt(paramMap.get("age"));
 
+
         Member member = new Member(username, age);
         memberRepository.save(member);
 
